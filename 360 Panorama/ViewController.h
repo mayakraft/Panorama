5fbcead96dbe1f06951e10c2aa2860e9d045ac6d
Robby Kraft <robby@Robbys-Mac-mini.local>
2013-08-24 00:16:59 -0700

    Initial Commit

---
+++ 360 Panorama/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  360 Panorama
+//
+//  Created by Robby Kraft on 8/24/13.
+//  Copyright (c) 2013 Robby Kraft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ 360 Panorama/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  360 Panorama
+//
+//  Created by Robby Kraft on 8/24/13.
+//  Copyright (c) 2013 Robby Kraft. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
